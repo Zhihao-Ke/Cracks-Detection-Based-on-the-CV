ae4a974b7e932a5eec306325299c36ea55dd4b8a
Zhihao-Ke <42312343+Zhihao-Ke@users.noreply.github.com>
2019-06-26 03:03:29 +0100

    Create Approach  1.py

---
+++ Python Code/Approach  1.py
@@ -0,0 +1,9 @@
+import cv2.cv as cv
+image = cv.LoadImage('11.jpg')
+grayimg= cv.CreateImage(cv.GetSize(image), image.depth, 1)
+for i in range(image.height):
+    for j in range(image.width):
+        grayimg[i,j] = max(image[i,j][0], image[i,j][1], image[i,j][2])
+cv.ShowImage('srcImage', image)        
+cv.ShowImage('grayImage', grayimg)
+cv.WaitKey(0)
